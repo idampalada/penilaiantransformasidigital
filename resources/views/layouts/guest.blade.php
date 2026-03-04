c9df0945ff4e8125e314ff149ef709d5b17256d9
idampalada <idampalada@if.uai.ac.id>
2026-03-04 14:11:51 +0700

    coba

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login — PUSDATIN</title>
+    <title>Login — Transformasi Digital</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
